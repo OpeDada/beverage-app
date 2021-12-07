d46a062426c285179153894f533a6f377de61963
OpeDada <opedada@gmail.com>
2021-12-07 14:44:39 +0900

    create an app file and import the routes

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -1,4 +1,7 @@
 import React from "react"
 import Routes from "../routes/Index"
 
+// export a component that renders the routes within fragments
+// this component will be rendered at the entry point of the app
+// thereby making the routes available when the app is loaded
 export default props => <>{Routes}</>
