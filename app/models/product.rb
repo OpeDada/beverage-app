b12fe9fe3e0e707b6fb18d715e207fdfafeb5883
OpeDada <opedada@gmail.com>
2021-12-08 14:55:44 +0900

    generate product model and add validations and associations

---
+++ app/models/product.rb
@@ -0,0 +1,7 @@
+class Product < ApplicationRecord
+  has_many :order_items, dependent: :destroy
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :description
+  validates :brand, presence: true
+end
