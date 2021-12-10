f3eda1aa1f18ec56819e60b017bb7daaaff03f07
OpeDada <opedada@gmail.com>
2021-12-10 15:03:15 +0900

    refactor home class

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -1,28 +1,26 @@
-import React from "react"
-import { Link } from "react-router-dom"
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
     <div className="vw-100 vh-100 primary-color d-flex align-items-center justify-content-center">
       <div className="jumbotron jumbotron-fluid bg-transparent">
         <div className="container secondary-color">
-      <h1 className="display-4">Shop by Brand</h1>
-      <p className="lead">Shop all products
-        </p>
-        <hr className="my-4" />
-      <Link
-      to="/beverages"
-      className="btn btn-lg custom-button"
-      role="button"
-      >
-        See list of products
-      </Link>
+          <h1 className="display-4">Shop by Brand</h1>
+          <p className="lead">Shop all products</p>
+          <hr className="my-4" />
+          <Link
+            to="/beverages"
+            className="btn btn-lg custom-button"
+            role="button"
+          >
+            See list of products
+          </Link>
+        </div>
+      </div>
     </div>
-    </div>
-    </div>
-  )
+  );
 }
-
 
 // export default () => (
 //   <div className="vw-100 vh-100 primary-color d-flex align-items-center justify-content-center">
